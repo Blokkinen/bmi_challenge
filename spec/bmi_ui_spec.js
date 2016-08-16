aaa590b0920d83051bbf5a6f798828c99ad6bff1
blokkinen <viktoria_blok@hotmail.com>
2016-08-16 16:00:22 +0200

    added a Gemfile, config and server

--- spec/bmi_ui_spec.js
+++ spec/bmi_ui_spec.js
@@ -3,6 +3,16 @@ describe('BMI_UI - index.html', function() {
         jasmine.getFixtures().fixturesPath = '.';
         loadFixtures('index.html');
         $.holdReady(false);
+        $('#weight').val('68');
+        $('#height').val('153');
+        $('#calculate').trigger('click');
     });
 
+    it("displays BMI Value", function() {
+        expect($('#display_value').text()).toBe('Your BMI is 29.05');
+    });
+
+    it("displays BMI Message", function() {
+        expect($('#display_message').text()).toBe('and you are Overweight');
+    });
 });
